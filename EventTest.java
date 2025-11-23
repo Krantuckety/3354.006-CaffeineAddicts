d0e670d66923b1723f6e1acbeb54a3d5144df896
Krantuckety <nferenczhalmy@gmail.com>
2025-11-23 06:02:16 -0600

    WiP EventTests
    extra tests for EventTest testing invalid input for Category, abnormal inputs for title. Commented out until finished.

--- EventTest.java
+++ EventTest.java
@@ -38,8 +38,8 @@ public class EventTest
 					LocalDate.of(2025, 11, 18),     // date
 					21,                             // startHour
 					0,                              // startMinute
-					19,                             // endHour
-					30,                             // endMinute
+					19,                             // INVALID endHour
+					30,                             // INVALID endMinute
 					"Personal",                     // category
 					true,                           // weekly recurrence
 					"2 hours before"                // alert string
@@ -48,6 +48,71 @@ public class EventTest
 		} catch (IllegalArgumentException e) {
 			assertEquals("End time must be after start time.", e.getMessage());
 		}
-
 	}
+
+	/*@Test
+	public void testInvalidCategory()
+	{
+		try 
+    	{
+        	Event e = new Event(
+                "Mystery Hunt",				// title
+                LocalDate.of(2025, 9, 16),	// date
+                12,							// startHour
+                30,							// startMinute
+                17,							// endHour
+                0,							// endMinute
+                "Mystery",      			// INVALID category
+                true,						// weekly recurrence
+                "1 hour before"				// alert string
+        	);
+        	fail("Expected IllegalArgumentException for invalid category.");
+    	} 
+    	catch (IllegalArgumentException e) 
+    	{
+        	assertTrue(e.getMessage().contains("Invalid category"));
+    	}
+	}
+
+	// Helper constructor to be used in testAbnormalTitles()
+	public Event createEventUniqueTitle(String title)
+	{
+		Event e = new Event(
+			title,						// title FROM PARAMETER
+			LocalDate.of(2025, 4, 4),	// date
+            13,							// startHour
+            0,							// startMinute
+            14,							// endHour
+            45,							// endMinute
+            "Work",		      			// INVALID category
+            false,						// weekly recurrence
+            "1 hour before"				// alert string
+		)
+		return e;
+	}
+	
+	@Test
+	public void testAllNumberTitle()
+	{
+		Event e1 = createEventUniqueTitle("1234");
+		assertEquals("This is a valid title composed of all numbers", "1234", e.getTitle());
+	}
+	
+	@Test
+	public void testAllSpecialCharsTitle()
+	{
+		Event e1 = createEventUniqueTitle("@+^");
+		assertEquals("This is a valid title composed of all special characters", "@+^", e.getTitle());
+	}
+
+	@Test
+	public void testVeryLongTitle()
+	{
+		Event e1 = createEventUniqueTitle("aaaaabbbbbcccccdddddeeeeefffffggggghhhhhijklmnopqrstuvwxyz");
+		assertEquals("This is a valid title of a length over 50", 
+					 "aaaaabbbbbcccccdddddeeeeefffffggggghhhhhijklmnopqrstuvwxyz", e.getTitle()
+					);
+	}*/
+
+	
 }
